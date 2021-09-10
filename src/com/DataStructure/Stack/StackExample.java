15293097c8e701949e25578a114e3793fd81a988
ksuryawa <kapil.r.suryawanshi@gmail.com>
2021-09-10 13:51:29 +0100

    Updates Array Example code

--- src/com/DataStructure/Stack/StackExample.java
+++ src/com/DataStructure/Stack/StackExample.java
@@ -2,7 +2,7 @@ package com.DataStructure.Stack;
 
 public class StackExample {
     public static void main(String[] args) {
-        Stack stack=new Stack(6);
+        Stack stack = new Stack(6);
 
         stack.push(17);
         stack.push(20);
@@ -18,8 +18,8 @@ public class StackExample {
 
         System.out.println("\n**************************\n");
         System.out.print("Poped Items :: ");
-        System.out.print( stack.pop() + " ");
-        System.out.print( stack.pop()+ " ");
+        System.out.print(stack.pop() + " ");
+        System.out.print(stack.pop() + " ");
 
         System.out.println("\n\n*** Stack After Pop ***");
         stack.display();
